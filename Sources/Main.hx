556a1eeac0f4df4194f7a99a78fb4fb8fe3cc0f4
nanjizal <nitsuj@nanjizal.net>
2017-03-07 10:14:44 +0000

    add anti-alias

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -4,7 +4,7 @@ import kha.System;
 
 class Main {
 	public static function main() {
-		System.init({title: "Empty", width: 800, height: 600}, function() {
+		System.init({title: "Empty", width: 800, height: 600, samplesPerPixel: 3 }, function() {
 			new Empty();
 		});
 	}
